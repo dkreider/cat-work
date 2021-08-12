aa8b6107c961baba05e18b3fed9d9995e7973204
Daniel Kreider <daniel@danielk.tech>
2021-08-12 17:18:57 -0300

    Add our cat service

---
+++ src/app/shared/services/cats.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { Cat } from '../models/cat';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CatsService {
+
+  private readonly catsURL = `${environment.apiServer}/cats`;
+
+  constructor(private httpClient: HttpClient) { }
+
+  getCats(): Observable<Cat[]> {
+    return this.httpClient.get<Cat[]>(this.catsURL);
+  }
+}
