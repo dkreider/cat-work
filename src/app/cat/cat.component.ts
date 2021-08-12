7fca5153497bf27f7b52b1661554de3025f9e469
Daniel Kreider <daniel@danielk.tech>
2021-08-12 17:38:14 -0300

    Working on the cat component

--- src/app/cat/cat.component.ts
+++ src/app/cat/cat.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Cat } from '../shared/models/cat';
+import { CatsService } from '../shared/services/cats.service';
 
 @Component({
   selector: 'app-cat',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatComponent implements OnInit {
 
-  constructor() { }
+  cat: Cat = {} as Cat;
+
+  constructor(
+    private catsService: CatsService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.loadCat();
+  }
+
+  loadCat() {
+    let id = this.route.snapshot.paramMap.get("id");
+    this.catsService.getCat(id).subscribe(c => this.cat = c);
   }
 
 }
