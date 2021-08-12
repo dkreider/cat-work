21c9e57d0ec26132b5cceb787e072117a488fd0b
Daniel Kreider <daniel@danielk.tech>
2021-08-12 17:19:31 -0300

    Display our cats

--- src/app/cats/cats.component.ts
+++ src/app/cats/cats.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Cat } from '../shared/models/cat';
+import { CatsService } from '../shared/services/cats.service';
 
 @Component({
-  selector: 'app-cats',
+  selector: 'cw-cats',
   templateUrl: './cats.component.html',
   styleUrls: ['./cats.component.css']
 })
 export class CatsComponent implements OnInit {
 
-  constructor() { }
+  cats: Cat[] = [];
+
+  constructor(private catsService: CatsService) { }
 
   ngOnInit(): void {
+    this.loadCats();
+  }
+
+  loadCats() {
+    this.catsService.getCats().subscribe(cats => this.cats = cats);
   }
 
 }
