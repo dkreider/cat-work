f2acf7d8209a984514a490dbd6523aa913246490
Daniel Kreider <daniel@danielk.tech>
2021-08-14 12:14:33 -0300

    Add tests for trim pipe

--- src/app/shared/pipes/trim.pipe.spec.ts
+++ src/app/shared/pipes/trim.pipe.spec.ts
@@ -5,4 +5,13 @@ describe('TrimPipe', () => {
     const pipe = new TrimPipe();
     expect(pipe).toBeTruthy();
   });
+  it('should return Hello World...', () => {
+    let expected, actual;
+    const pipe = new TrimPipe();
+
+    expected = "Hello World...";
+    actual = pipe.transform("Hello World How Are You", 11);
+
+    expect(actual).toEqual(expected);
+  })
 });
