1fdae037d1253f1a949341b431c0be129c35a298
Daniel Kreider <daniel@danielk.tech>
2021-09-10 08:48:11 -0300

    Use new directive to show favorite cats

--- src/app/cats/cats.module.ts
+++ src/app/cats/cats.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { CatsRoutingModule } from './cats-routing.module';
 import { CatsComponent } from './cats.component';
 import { PipesModule } from '../shared/pipes/pipes.module';
+import { StaredDirective } from './stared.directive';
 
 
 @NgModule({
   declarations: [
-    CatsComponent    
+    CatsComponent,
+    StaredDirective    
   ],
   imports: [
     CommonModule,
