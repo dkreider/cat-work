975f99a6b2c82784d32c0142b2d0cbb3a66b00c1
Daniel Kreider <daniel@danielk.tech>
2021-09-13 12:30:52 -0300

    Add tests for stared directive

--- src/app/cats/stared.directive.spec.ts
+++ src/app/cats/stared.directive.spec.ts
@@ -1,6 +1,29 @@
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { StaredDirective } from './stared.directive';
 
+@Component({
+  template: `<i [stared]="false"></i>`
+})
+class TestComponet {};
+
 describe('StaredDirective', () => {
+  let fixture: ComponentFixture<TestComponet>;
+
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({
+      declarations: [TestComponet, StaredDirective]
+    })
+    .createComponent(TestComponet);
+
+    fixture.detectChanges();
+  });
+
+  it('should be stared', () => {
+    var element: HTMLElement = fixture.nativeElement.querySelector("i");
+    expect(element.className).toEqual('bi-star-fill');
+  })
+
   it('should create an instance', () => {
     const directive = new StaredDirective();
     expect(directive).toBeTruthy();
